482405a7dd74c3e3df383bfe6c037e6ff3862499
Saumitra Bagchi <saumitra.bagchi@ericsson.com>
2021-04-03 07:15:35 +0300

    quiz  author

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,102 @@
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
+const Hero =()=>{
+  return(
+    <div className="row">
+    <div className=" col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p>Select the book written by the author shown</p>
+     </div>
+     </div>
+  );
+
+}
+function Book({title, onClick}){
+  return(
+    <div className="answer" onClick={()=>
+        {
+          onClick(title);
+        }
+    }>
+      <h4>{title}</h4>
+    </div>
+  )
+
+}
+const Turn =({author, books,highlight,onAnswerSelected})=>{
+
+  function highlightBackgroundColor(highlight){
+    const mapping ={
+      'none': '',
+      'correct': 'green',
+      'wrong': 'red'
+    };
+    return mapping[highlight];
+  }  
+  return(
+    <div className="row turn" style={{backgroundColor:highlightBackgroundColor(highlight)}}>
+      <div className=" col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="Author"/>
+      </div>
+      <div className="col-6">
+        {books.map((title)=><Book title={title} key={title} onClick={onAnswerSelected}/>)}
+      </div>
+     </div>
+  );
+
+}
+Turn.propTypes={
+  author: PropTypes.shape({
+    name:PropTypes.string.isRequired, 
+    imageUrl:PropTypes.string.isRequired,
+		imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf( PropTypes.string).isRequired,
+    
+  }),
+  books: PropTypes.arrayOf( PropTypes.string).isRequired,
+  onAnswerSelected:PropTypes.func.isRequired,
+  highlight:PropTypes.string
+  
+};
+
+function Continue ({show,onContinue}){
+  return(
+    <div  className="row continue">
+      {show ?
+      <div className="col-11">
+        <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button>
+      </div>
+      : null}
+      
+    </div>
+   
+  );
+
+}
+const Footer =()=>{
+  return(
+    <div id="footer" className="row turn">
+    <div className=" col-2">
+      <p className="text-muted credit">All images are from <a href="https://commons.wikimedia.org/wiki/Main_Page"> Wikimedia Commons</a></p>
+     </div>
+     </div>
+  )
+
+}
+function AuthorQuiz({turnData,highlight,onAnswerSelected,onContinue}) {
+  return (
+    <div className="container-fluid" >
+      <Hero/>
+      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
+      <Continue show={highlight==='correct'} onContinue={onContinue}/>
+      <p><Link to="/add">Add an author</Link></p>
+      <Footer/>
+    </div>
+  );
+}
+
+export default AuthorQuiz;
---
+++ src/index.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AuthorQuiz from './AuthorQuiz';
+import reportWebVitals from './reportWebVitals';
+import {shuffle,sample} from 'underscore';
+import {BrowserRouter,Route,withRouter} from 'react-router-dom'
+import AddAuthorForm from './AddAuthorForm'
+const authors=[
+  {
+		name: 'Mark Twain',
+		imageUrl: 'images/authors/marktwain.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: [ 'The Adventures of Huckleberry Finn' ]
+	},
+	{
+		name: 'Joseph Conrad',
+		imageUrl: 'images/authors/marktwain.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: ['Heart of Darkness']
+	},
+	{
+		name: 'J.K. Rowling',
+		imageUrl: 'images/authors/jkrowling.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: ['Harry Potter and the Sorcerers Stone']
+	},
+	{
+		name: 'Stephen King',
+		imageUrl: 'images/authors/stephenking.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: ['The Shining', 'IT']
+	},
+	{
+		name: 'Charles Dickens',
+		imageUrl: 'images/authors/charlesdickens.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: ['David Copperfield', 'A Tale of Two Cities']
+	},
+	{
+		name: 'William Shakespeare',
+		imageUrl: 'images/authors/williamshakespeare.jpg',
+		imageSource: 'Wikimedia Commons',
+		books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+	},
+
+];
+function getTurnData(){
+  const allBooks=authors.reduce(function(p,c,i){
+    return p.concat(c.books);
+   
+  }, []);
+  const fourRandomBooks=shuffle(allBooks).slice(0,4);
+  const answer= sample(fourRandomBooks);
+  return{
+    books: fourRandomBooks,
+    author: authors.find((author)=>
+      author.books.some((title)=>
+      title===answer))
+  }
+}
+
+function resetState(){
+return {
+	
+	turnData:getTurnData(authors),
+	highlight: 'wrong'
+	  
+};
+}
+let state=resetState()
+function onAnswerSelected(answer){
+  const IsCorrect= state.turnData.author.books.some((book) => book === answer);
+  state.highlight= IsCorrect ? 'correct' : 'wrong';
+  render ();
+}
+
+function App(){
+	
+	return (
+	<AuthorQuiz {...state} 
+	onAnswerSelected={onAnswerSelected}
+	onContinue={()=>{
+		state=resetState();
+		render();
+	}}
+	/>
+
+	) 
+	
+}
+const AddAuthorWrapper= withRouter(({history})=>{
+	
+	return (
+	<AddAuthorForm onAddAuthor ={(author)=>{
+		authors.push(author);
+		history.push('/');
+	}}/>
+	) ;
+
+})
+
+
+function render(){
+  ReactDOM.render(
+	  <BrowserRouter>
+	  <React.Fragment>
+	   <Route  exact path="/" component={App}/>
+	   <Route   path="/add" component={AddAuthorWrapper}/>
+	   </React.Fragment>
+	  </BrowserRouter> ,document.getElementById('root'));
+}
+render ();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
